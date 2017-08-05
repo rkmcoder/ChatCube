7f17df268761f3f794c52cafcfdbf509e3cc07af
Rakesh Kumar. Mallick <rkmallic@oradev.oraclecorp.com>
2017-08-06 01:09:48 +0530

    work in progress external js not working

--- chatcube.js
+++ chatcube.js
@@ -1,22 +1,26 @@
 var express = require("express");
 var http = require("http");
 var app = express();
-var server = http.createServer(app).listen(3000);
+var server = http.createServer(app);
 var io = require("socket.io")(server);
+server.listen(process.env.PORT || 3000); 
+var connections = [];
+var users =[];
+//app.use(express.static("./public"));
+app.get("/", function(req,res){
+ 	res.sendFile(__dirname + '/public/index.html');
+ });
 
-app.use(express.static("./public"));
+console.log("Server running...on port 3000");
 
-io.on("connection",function(socket){
-    
-    console.log("fucntion called");
-    socket.on("chat", function(message) {
-    	socket.broadcast.emit("message", message);
-    });
-    
-    console.log("fucntion called 2 times");
-  
+io.sockets.on('connect', function(socket){
     socket.emit("message","Welcome to Chat Cube");
-
+    connections.push(socket);
+    console.log('No. of users connected %s connections.length');
 });
 
-console.log("Starting Socket App - http://localhost:3000");
+io.sockets.on('disconnect',function(socket){
+    connections.splice(connections.indexOf(socket),1);
+    console.log('No. of users still connected %s connections.length');
+});
+
