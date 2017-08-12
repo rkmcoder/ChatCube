5feb0f93491773ed2246c1d8c147640ebf5de634
Rakesh Kumar. Mallick <rkmallic@oradev.oraclecorp.com>
2017-08-12 20:44:24 +0530

    external js working

--- chatcube.js
+++ chatcube.js
@@ -1,26 +1,30 @@
 var express = require("express");
 var http = require("http");
 var app = express();
-var server = http.createServer(app);
+var server = http.createServer(app).listen(3000);
 var io = require("socket.io")(server);
-server.listen(process.env.PORT || 3000); 
+
 var connections = [];
 var users =[];
-//app.use(express.static("./public"));
-app.get("/", function(req,res){
- 	res.sendFile(__dirname + '/public/index.html');
- });
+app.use(express.static("./public"));
+// app.get("/", function(req,res){
+//  	res.sendFile(__dirname + '/public/index.html');
+
+//  });
 
 console.log("Server running...on port 3000");
 
 io.sockets.on('connect', function(socket){
+	socket.on("message", function(message) {
+    	socket.broadcast.emit("message", message);
+    });
     socket.emit("message","Welcome to Chat Cube");
     connections.push(socket);
-    console.log('No. of users connected %s connections.length', connections.length);
+     console.log('No. of users connected %s', connections.length);
 });
 
 io.sockets.on('disconnect',function(socket){
     connections.splice(connections.indexOf(socket),1);
-    console.log('No. of users still connected %s connections.length', connections.length);
+     console.log('No. of users still connected %s', connections.length);
 });
 
